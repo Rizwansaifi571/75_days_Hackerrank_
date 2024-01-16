25b199abc8d9d0a2b9a0f7c9a0fecdacf495031d
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-01-16 23:24:02 +0530

    Add file Via Git

---
+++ Day_1/problem_2.py
@@ -0,0 +1,21 @@
+# Date : 16,Jan,2024
+
+'''
+Given the names and grades for each student in a class 
+of N students, store them in a nested list and print the name(s)
+ of any student(s) having the second lowest grade.
+
+Note: If there are multiple students with the second lowest 
+grade, order their names alphabetically and print each name on a new line.
+
+Explanation 0 :
+There are 5 students in this class whose names and grades are 
+assembled to build the following list:
+
+python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], 
+['Akriti', 41], ['Harsh', 39]]
+
+The lowest grade of 37.21 belongs to Tina. The second lowest grade of 37.21 
+belongs to both Harry and Berry, so we order their names alphabetically and print each name on a new line.
+
+'''
