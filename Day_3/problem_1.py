6fd7c111d377dc28ada01d11530aa29635f330d7
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-01-18 21:40:51 +0530

    Add file Via a Git

---
+++ Day_3/problem_1.py
@@ -0,0 +1,21 @@
+# Date : 18,Jan,2024
+
+'''
+Calendar Module
+The calendar module allows you to output calendars and provides additional
+useful functions for them.
+
+class calendar.TextCalendar([firstweekday])
+
+This class can be used to generate plain text calendars.
+
+Task:
+You are given a date. Your task is to find what the day is on that date.
+'''
+
+import calendar
+date = list(map(int, input('Enter :').split()))
+weekday_index = calendar.weekday(date[2], date[0], date[1])
+weekday_name = calendar.day_name[weekday_index]
+print(weekday_index)
+print(weekday_name.upper())
