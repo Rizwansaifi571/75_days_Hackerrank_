ee55cb0c511fb48591b2d4a4d60c9050a91bd015
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-01-18 23:24:49 +0530

    Add

---
+++ Day_3/problem_4.py
@@ -0,0 +1,14 @@
+# Date : 18,Jan,2024
+
+'''
+Sample Input :
+1222311
+
+Sample Output :
+(1, 1) (3, 2) (1, 3) (2, 1)
+
+Explanation :
+First, the character 1 occurs only once. It is replaced by (1,1) . Then the character 2 
+occurs three times, and it is replaced by (3,2) and so on.
+Also, note the single space within each compression and between the compressions.
+'''
